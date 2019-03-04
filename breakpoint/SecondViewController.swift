5ee91227459fddd03bb34692776560c514ea0a31
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-04 00:24:13 -0500

    Started project and added gitignore

---
+++ breakpoint/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  breakpoint
+//
+//  Created by Ricardo Herrera Petit on 3/4/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
