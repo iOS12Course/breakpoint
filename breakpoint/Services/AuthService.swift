69a80c66c8953e8610b76a97bff4872f801d120d
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-04 11:47:35 -0500

    Setting up AuthService

---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,40 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by Ricardo Herrera Petit on 3/4/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    
+    
+    func registerUser(withEmail email: String, andPassword password:String, userCreationComplete: @escaping (_ status:Bool, _ error:Error?) -> () ) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user  else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider" : user.providerID, "email" : user.email ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData as Dictionary<String, Any>)
+            userCreationComplete(true, nil)
+        }
+        
+    }
+    
+    func loginUser(withEmail email: String, andPassword password:String, loginComplete: @escaping (_ status:Bool, _ error:Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
