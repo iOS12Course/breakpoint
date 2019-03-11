680135eef246d78fb29f71e280c5fa22e5c36c0f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-10 23:38:45 -0500

    Presenting GroupFeedVC

---
+++ breakpoint/Extensions/UIViewControllerExt.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewControllerExt.swift
+//  breakpoint
+//
+//  Created by Ricardo Herrera Petit on 3/10/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        present(viewControllerToPresent, animated: false, completion: nil)
+    }
+    
+    
+    func dismissDetail() {
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window?.layer.add(transition, forKey: kCATransition)
+        
+        dismiss(animated: false, completion: nil)
+    }
+}
